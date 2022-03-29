c8b9cd854719ba02ba167e57c5fe6c8a8d73bb18
sanju <sanjusabu066@gmail.com>
2022-03-29 21:35:47 +0530

    register

--- node_js/server.js
+++ node_js/server.js
@@ -2,8 +2,10 @@ const { response } = require('express');
 
 const express = require('express');
 const { render } = require('express/lib/response')
-const app=express();
-
+const app = express();
+const sqlite3 = require('sqlite3')
+const bodyparser=require('body-parser')
+app.use(bodyparser.urlencoded({extended: true}));
 // const mysql=require('mysql');
 
 // const db=mysql.createConnection(
@@ -18,32 +20,72 @@ const app=express();
 let path = require('path');
 
 // app.use()
-console.log("Hello "+__dirname)
+console.log("Hello " + __dirname)
 app.use(express.static(__dirname + '/public'));
+const db_name = path.join(__dirname, "data", "register.db")
+console.log(db_name)
+const db = new sqlite3.Database(db_name, err => {
+    if (err) {
+        return console.log(err.message)
+    }
+    console.log("Database Connected")
+})
+
+const table = 'CREATE TABLE IF NOT EXISTS users(uid INTEGER PRIMARY KEY AUTOINCREMENT,username VARCHAR(100) NOT NULL,password varchar(300) NOT NULL);'
+db.run(table,err=>
+    {
+        if(err)
+        {
+            return console.log(err.message)
+        }
+        console.log("Table Created")
+    })
 
 
-app.get("/",(req,res)=>
-{
+
+app.get("/", (req, res) => {
     res.sendFile(path.resolve("./public/HTML/home.html"))
     // res.render(path.resolve("../public/HTML/home.html"), { title: "Home" });
 })
 
-app.get("/login",(req,res)=>
-{
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve("./public/HTML/login.html"))
 })
 
-app.get("/help",(req,res)=>
-{
+app.get("/help", (req, res) => {
     res.sendFile(path.resolve("./public/HTML/help.html"))
 })
 
-app.get("/register",(req,res)=>
-{
+app.get("/register", (req, res) => {
     res.sendFile(path.resolve("./public/HTML/register.html"))
 })
 
-app.listen(3010,'localhost',()=>
-    {
-        console.log("Server is running")
-    })
+app.post("/register",(req,res)=>
+{
+    // const username=req.body.username;
+    // const password=req.body.password;
+    console.log(req.body)
+    db.run('INSERT INTO users(username,password) VALUES(?,?)',[req.body.username,req.body.password],err=>
+        {
+            if(err)
+            {
+                return console.log(err.message)
+            }
+            console.log('Data Entered')
+        })
+        res.sendFile(path.resolve("./public/HTML/login.html"))
+        // const sql='SELECT * FROM users ORDER by uid'
+
+        // db.all(sql,(err,rows)=>
+        // {
+        //     if(err)
+        //         return console.log(err.message);
+        // }
+        
+        // )
+        
+})
+
+app.listen(3010, 'localhost', () => {
+    console.log("Server is running")
+})
