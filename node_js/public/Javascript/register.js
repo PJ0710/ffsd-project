1fb70eecb0dd78ffd7842c04c80422160a985845
sanju <sanjusabu066@gmail.com>
2022-03-29 21:41:03 +0530

    register_done

--- node_js/public/Javascript/register.js
+++ node_js/public/Javascript/register.js
@@ -1,17 +1,47 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-    
-      let forms = document.getElementsByClassName('needs-validation');
-    
-      let validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+function validateForm() {
+  let pw1 = document.getElementById("validationCustom03").value;
+  let pw2 = document.getElementById("validationCustom04").value;
+  let email = document.getElementById("validationCustom02").value;
+  let phone = document.getElementById("validationCustom05").value;
+  let username = document.getElementById("validationCustom01").value;
+
+  if (!isNaN(username)) {
+    document.getElementById("blankMsg").innerHTML =
+      "Only characters are allowed";
+    return false;
+  }
+
+  if (pw1.length < 8) {
+    document.getElementById("message1").innerHTML =
+      "Password length must be atleast 8 characters";
+    return false;
+  }
+
+  if (pw1.length > 15) {
+    document.getElementById("message1").innerHTML =
+      "Password length must not exceed 15 characters";
+    return false;
+  }
+
+  if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+    document.getElementById("message3").innerHTML = "Invalid Email Address";
+    return false;
+  }
+
+  if (phone.length != 10) {
+    document.getElementById("message4").innerHTML = "Invalid Phone Number";
+    return false;
+  }
+
+  if (pw1 != pw2) {
+    document.getElementById("message2").innerHTML = "Passwords are not same";
+    return false;
+  } else {
+    alert("Your Account Was Created Successfully!");
+  }
+}
+document.querySelector("#register").addEventListener("submit", function (e) {
+  if (!validateForm()) {
+    e.preventDefault();
+  }
+});
