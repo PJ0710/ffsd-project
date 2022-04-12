0680c4b4dbc541839b4fbe262e7ae3b9022a0efe
sanju <sanjusabu066@gmail.com>
2022-04-12 14:00:06 +0530

    removed comments

--- node_js/server.js
+++ node_js/server.js
@@ -42,8 +42,6 @@ app.get("/register", (req, res) => {
     res.render("register");
 })
 
-
-
 app.get("/Transactions", (req, res) => {
     res.render("Transactions");
 })
@@ -71,18 +69,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login',async(req,res)=>
 {
-// const check=details.findOne({username,password});
-// console.log(check);
-// var coll = details.collection('details');
 
-// coll.find({}).toArray(function (err, result) {
-//     if (err) {
-//         res.send(err);
-//     } else {
-
-//         res.send(JSON.stringify(result));
-// }
-// })
 const uname=req.body.username;
 const user=await details.findOne({username:uname});
 console.log(user.password);
@@ -90,6 +77,9 @@ if(user.password===req.body.password)
 {
     res.render("sidebar",{title:uname})
     // res.redirect("/Profile");
+    // app.get("/Profile", (req, res) => {
+    //     res.render("sidebar");
+    // })
     
 }
 else
@@ -105,102 +95,5 @@ app.listen(3010, 'localhost', () => {
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const db_name = path.join(__dirname, "data", "register.db")
-
-
-// const db = new sqlite3.Database(db_name, err => {
-//     if (err) {
-//         return console.log(err.message)
-//     }
-//     console.log("Database Connected")
-// })
-
-// const table = 'CREATE TABLE IF NOT EXISTS users(username VARCHAR(100) PRIMARY KEY NOT NULL,password varchar(300));'
-// db.run(table,err=>
-//     {
-//         if(err)
-//         {
-//             return console.log(err.message)
-//         }
-//         console.log("Table Created")
-//     })
-
-
-
-
-// app.post("/register",(req,res)=>
-// {
-//     // const username=req.body.username;
-//     // const password=req.body.password;
-//     console.log(req.body)
-
-//     db.run('INSERT INTO users(username,password) VALUES(?,?)',[req.body.username,req.body.password],err=>
-//         {
-//             if(err)
-//             {
-//                 return console.log(err.message)
-//             }
-//             console.log('Data Entered')
-//         })
-//         res.redirect('/login')
-
-// })
-
-
-// app.post('/login', (req, res)=>{
-
-//     const username = req.body._username;
-//     const password = req.body._password;
-
-//     console.log(+req.body)
-
-//     db.get("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], (err, row)=>{
-//         if(err){
-//             console.log("Error in login"+ err)
-//         }
-//         else{
-//             if(row){
-//                 res.redirect('/Profile');
-//             }
-//             else{
-//                 console.log(req.body);
-
-//                 console.log("Invalid username/password");
-//             }
-//         }
-//     });
-
-// });
 
 
