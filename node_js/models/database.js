1812274524ed7ab29775b49c966bf9aa92ea5d7c
sanju <sanjusabu066@gmail.com>
2022-04-13 16:03:30 +0530

    server/tokens

--- node_js/models/database.js
+++ node_js/models/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
-
+const jwt = require("jsonwebtoken");
 const template=new Schema(
     {
         username:{
@@ -21,12 +21,33 @@ const template=new Schema(
         mobile:{
             type:Number
 
-        }
+        },
 
+        tokens:[{
+            token:{
+                type:String
+                
+            }
+        }]
     
     },   { timestamps:true }
 );
 
+template.methods.generateAuthtoken = async function()
+{
+    try{
+        const tok = jwt.sign({_id:this._id.toString()},"mynameissanjusabuandiamacseundergraduatestudent")
+        // console.log(token);
+        this.tokens = this.tokens.concat({token:tok});
+        console.log(this.tokens);
+        await this.save();
+        return tok;
+    }
+    catch(error)
+    {
+        console.log("The error part "+ error);
+    }
+}
 const details=mongoose.model('details',template);
 
 module.exports=details;
--- node_js/server.js
+++ node_js/server.js
@@ -8,6 +8,8 @@ const path = require('path');
 const Det = require('./models/database');
 const bodyparser = require('body-parser');
 const details = require('./models/database');
+// const session = require("express-session");
+// const MongoDBSession = require('connect-mongodb-session')(session);
 
 app.use(bodyparser.urlencoded({ extended: true }));
 // app.use()
@@ -20,17 +22,37 @@ mong.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(()
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("home");
-    
-})
-
 app.get("/Profile", (req, res) => {
     res.render("sidebar");
-    
+
 })
 
+// const store = new MongoDBSession(
+//     {
+//        uri : dbUrl,
+//        collection:'Session_cookies',
+//     }
+// )
+
+// app.use(session({
+//     secret: 'key that will sign cookie',
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store,
+// }))
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+
+})
+
+
 app.get("/login", (req, res) => {
+
+    // req.session.isAuth = true;
+    // console.log(req.session);
+    // console.log(req.session.id);
     res.render("login");
 })
 
@@ -50,42 +72,64 @@ app.get("/aboutus", (req, res) => {
     res.render("aboutus");
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
 
-    const data=new details(
+    const data = new details(
         {
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-            mobile:req.body.mobile
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            mobile: req.body.mobile
         }
     )
-
-    data.save().then(res.redirect('/login')).catch((err)=>{
-        console.log(err);
-    })
+    const user = await details.findOne({ username: req.body.username });
+    if (!user) {
+        const token = await data.generateAuthtoken();
+        console.log('Register-token part ' + token);
+        // const registered=await;
+        data.save().then(res.redirect('/login')).catch((err) => {
+            console.log(err);
+        })
+    }
+    else
+    {
+        console.log("User Registered Already or username already exists");
+        res.redirect("/login");
+    }
 
 })
 
-app.post('/login',async(req,res)=>
-{
+// const isAuth = (req,res,next) => {
+//     if(req.session.isAuth)
+//     {
+//         next()
+//     }
+//     else{
+//         res.redirect("/login")
+//     }
+// }
 
-const uname=req.body.username;
-const user=await details.findOne({username:uname});
-console.log(user.password);
-if(user.password===req.body.password)
-{
-    res.render("sidebar",{title:uname})
-    // res.redirect("/Profile");
-    // app.get("/Profile", (req, res) => {
-    //     res.render("sidebar");
-    // })
-    
-}
-else
-{
-    res.render("login")  
-}
+app.post('/login', async (req, res) => {
+
+    const uname = req.body.username;
+    const user = await details.findOne({ username: uname });
+    // console.log(user.password);
+    const token = await user.generateAuthtoken();
+    console.log('Login- token part ' + token);
+
+    if (user.password === req.body.password) {
+
+        // req.session.isAuth=true;
+        res.render("sidebar", { title: uname })
+        // res.redirect("/Profile");
+        // app.get("/Profile", (req, res) => {
+        //     res.render("sidebar");
+        // })
+
+    }
+    else {
+        res.redirect("/login")
+    }
 
 })
 
