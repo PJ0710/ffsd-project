4464682e9a5d8dbe7a392753f53af812a8a46a2f
sanju <sanjusabu066@gmail.com>
2022-04-27 04:00:17 +0530

    admin/server/sidebar

--- node_js/models/admin.js
+++ node_js/models/admin.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const passportLocalMongoose=require("passport-local-mongoose");
 const Schema=mongoose.Schema;
 // create an schema
-var userSchema = new Schema({
+let userSchema = new Schema({
+    
             name: String,
             password: String,
-            email:String
+            
         });
 
-userSchema.plugin(passportLocalMongoose);
-var Users=mongoose.model('users',userSchema);
+let Users=mongoose.model('users',userSchema);
  
 module.exports = Users;
