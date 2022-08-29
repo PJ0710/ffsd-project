c68e59614b73a66249c7421397bf98b1482a0605
Pranjay Gupta <pranjay.g20@iiits.in>
2022-08-29 15:02:37 +0530

    formated js code

--- node_js/public/Javascript/register.js
+++ node_js/public/Javascript/register.js
@@ -1,24 +1,23 @@
 function validatepassword() {
-    let pw1 = document.getElementById("validationCustom03").value;
-    let pw2 = document.getElementById("validationCustom04").value;
-    if (pw1 != pw2) {
-
-        document.getElementById("message2").innerHTML = "Passwords are not same";
-        return false;
-    } else {
-        document.getElementById("message2").innerHTML = "";
-        return true;
-    }
+  let pw1 = document.getElementById("validationCustom03").value;
+  let pw2 = document.getElementById("validationCustom04").value;
+  if (pw1 != pw2) {
+    document.getElementById("message2").innerHTML = "Passwords are not same";
+    return false;
+  } else {
+    document.getElementById("message2").innerHTML = "";
+    return true;
+  }
 }
 
 function validateForm() {
-    if (validatepassword()) {
-        document.getElementById("bt").disabled = false;
+  if (validatepassword()) {
+    document.getElementById("bt").disabled = false;
 
-        return true;
-    } else {
-        document.getElementById("bt").disabled = true;
+    return true;
+  } else {
+    document.getElementById("bt").disabled = true;
 
-        return false;
-    }
+    return false;
+  }
 }
